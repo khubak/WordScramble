485709cff0012f639b12f8be633550f9804ce3a8
khubak <103487011+khubak@users.noreply.github.com>
2022-11-30 09:03:40 +0100

    WordScramble

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Karlo Hubak on 28.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
